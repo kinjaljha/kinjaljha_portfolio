6639781171cbf5d102957a9397841c5fc5bf132b
Kinjal Jha <kinjaljha3@gmail.com>
2020-05-04 00:32:54 +0530

    adding projects for the company

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -54,9 +54,48 @@ export default class Header extends Component {
                     Software Engineer <span>•</span> <em className='date'>June 2019 - Present</em>
                   </p>
                   <p>
-                    Worked in backend API designing and development using Nodejs, MongoDB, Redis CLI, AWS(Python Lambda Service). Currently working on
-                    Industrial Internet Of Things project for refrigeration devices of comercial/residential devices using React JS, Redux, Web
-                    Services, MongoDB, MQTT, Google Location API.
+                    <h6>IIot Device Management System for Refrigeration</h6>
+                    <p>
+                      This is a Product from an American privately held manufacturer that produces residential and commercial water heaters and
+                      boilers, as well as heating, ventilating and air conditioning equipment. We have developed purely one stop IOT solution to our
+                      client it includes frontend, backend, mobile app development and smart home skill development using an IOT platform our backend
+                      part is based on IOT standards and deployed on IOT platform only and the end user are using different functionalities developed
+                      by using Smart home devices or mobile devices. This product includes a frontend for both our customers and their clients.
+                      Product functionality includes Temperature control, Device power control, Device Provisioning, Device leak detection, Admin
+                      control, Third party Partner integration and Alexa and google smart home skills to support voice control.{' '}
+                    </p>
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Designing, implementation of web application in React and Redux
+                    <br />
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Implemented API services, Jenkins and AWS S3 configuration
+                    <br />
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Implementation of Authorisation Module, Device provisioning flow,
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Google Location API integration, Notification, Device handling by MQTT, Device Usage
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Graph(Chart.js) and Device Module
+                  </p>
+                  <p>
+                    <h6>Resource Skill Management Portal</h6>
+                    <p>
+                      A RSM is a type of packaged management software which handles persistent, unified employees skill set and technology stack data.
+                      Any organisation who desires to maintain employee skill set, evaluate employees performance can opt for this management
+                      software. Modules like authentication, authorisation, role based access to the system(trainee to CTO, CEO) is being implemented.
+                      Functionalities like importing/exporting existing skill data, creating a new one(CRUD) is possible.
+                    </p>
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Built User, Token, and Role Management Module for the Portal
+                    <br />
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Used Redis, MongoDB, and Node.js. Wrote unit and integration tests in Chai, Mocha
+                  </p>
+                  <p>
+                    <h6>Safety Device Project(IIot)</h6>
+                    <p>
+                      Client is an American safety brand company. They make carbon monoxide detectors, smoke alarms, fire extinguishers, and other
+                      safety products like flashlights and fire escape ladders.
+                    </p>
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Worked on device shadow parameters (Clover and prime AVS) from device <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;through AWS lambda (python)
+                    <br />
+                    <span>•</span>&nbsp;&nbsp;&nbsp;&nbsp;Refactored the logic for shadow params once the device gets on-boarded
                   </p>
                 </div>
               </div>{' '}
@@ -66,21 +105,21 @@ export default class Header extends Component {
                   <h3>Infosys Limited</h3>
                   <p className='info'>
                     Systems Engineer <span>•</span> <em className='date'>July2017 – June 2019</em>
+                    <span>•</span> Pune, India
                   </p>
                   <p>
-                    System Engineer - Infosys Limited(Pune, India)
                     <ul>
                       <li>
-                        Finacle Solutions: Worked on Finacle Core Banking Solutions by Edge Verve Ltd, as a part of FEBA Customization team, worked
-                        with Java and Unix
+                        <h6>Finacle Solutions</h6>
+                        Worked on Finacle Core Banking Solutions by Edge Verve Ltd, as a part of FEBA Customization team, worked with Java and Unix
                       </li>
                       <li>
-                        Deutsche Bank Project: Worked in Front End Automation, handled OCC and I-Cape applications. Worked with Java, Eclipse, BDD
-                        Cucumber, ALM
+                        <h6>Deutsche Bank Project</h6>
+                        Worked in Front End Automation, handled OCC and I-Cape applications. Worked with Java, Eclipse, BDD Cucumber, ALM
                       </li>
                       <li>
-                        Proximus Solutions: Implemented SV module which accepts device installation details(XML) and create response based on nearest
-                        service provider
+                        <h6>Proximus Solutions</h6>
+                        Implemented SV module which accepts device installation details(XML) and create response based on nearest service provider
                       </li>
                     </ul>
                   </p>
@@ -106,10 +145,8 @@ export default class Header extends Component {
                 <h5>Databases</h5>
                 MySQL, MongoDB, Redis<br></br>
                 <h5>Tools</h5>
-                Git, Jenkins, JIRA, Eclipse IDE, Android Studio, NetBeans IDE, 
-                Visual Studio, Soap UI, Ca-lisa, ALM, Parasoft Virtualize
-                , Pycharm, BDD Cucumber, Latex, Toad, MATLAB, AWS
-
+                Git, Jenkins, JIRA, Eclipse IDE, Android Studio, NetBeans IDE, Visual Studio, Soap UI, Ca-lisa, ALM, Parasoft Virtualize , Pycharm,
+                BDD Cucumber, Latex, Toad, MATLAB, AWS
                 <br></br>
               </p>
               {/* <div className="bars">
